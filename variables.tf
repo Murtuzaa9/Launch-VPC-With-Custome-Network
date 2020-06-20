e3252dacf2de61a3aa3fcff88e45027903bab08e
Murtuzaa9 <66246660+Murtuzaa9@users.noreply.github.com>
2020-06-20 11:32:52 +0530

    Update variables

--- variables.tf
+++ variables.tf
@@ -1,33 +1,44 @@
 variable "availabilityZone" {
-     default = "us-east-1a"
+  default = "us-east-1a"
 }
 variable "instanceTenancy" {
-    default = "default"
+  default = "default"
 }
 variable "dnsSupport" {
-    default = true
+  default = true
 }
 variable "dnsHostNames" {
-    default = true
+  default = true
 }
 variable "vpcCIDRblock" {
-    default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 variable "subnetCIDRblock" {
-    default = "10.0.1.0/24"
+  default = "10.0.1.0/24"
 }
 variable "destinationCIDRblock" {
-    default = "0.0.0.0/0"
+  default = "0.0.0.0/0"
 }
 variable "ingressCIDRblock" {
-    type = list
-    default = [ "0.0.0.0/0" ]
+  type    = list
+  default = ["0.0.0.0/0"]
 }
 variable "egressCIDRblock" {
-    type = list
-    default = [ "0.0.0.0/0" ]
+  type    = list
+  default = ["0.0.0.0/0"]
 }
 variable "mapPublicIP" {
-    default = true
+  default = true
 }
 # end of variables.tf
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default     = "ami-098f16afa9edf40be"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "t2.small"
+}
+variable "users" {
+  default = "ec2-user"
+}
